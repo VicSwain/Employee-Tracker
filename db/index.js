f9fad6abe1c1e78745c821c18c28d82045950d50
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-03-16 19:28:18 -0500

    error in searhEmployeesByDepartment

--- db/index.js
+++ db/index.js
@@ -13,12 +13,23 @@ class DB {
     }
   }
 
-
-  findAllEmployees() {
+  searchAllEmployees() {
     return this.query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
+        "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.dept_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.dept_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
     );
   }
+  
+  searchEmployeesByDepartment(dept_id) {
+    return this.query(
+        'SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department ON role.dept_id = dept.id WHERE dept.id = $1;',
+        [dept_id]
+    );
+  }    
 }
 
   module.exports = new DB();
+
+
+
+
+//   "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
