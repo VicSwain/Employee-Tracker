9815f3be45e1703b149af1828b7c3ae1c562b393
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-03-26 13:42:56 -0500

    updated README and comments for submission

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const pool = require('./connection');
 
 class DB {
   constructor() {}
-
+// async function to  execute SQL queries
   async query(sql, args = []) {
     const client = await pool.connect();
     try {
@@ -13,28 +13,29 @@ class DB {
     }
   }
 
+//function to search all employees
   searchAllEmployees() {
     return this.query(
         "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.dept_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.dept_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
     );
   }
-  
+
+//function to search all departments
   searchAllDepartments() {
     return this.query('SELECT department.id, department.dept_name FROM department;');
   }
-  
+
+//function to search all roles
   searchAllRole() {
     return this.query('SELECT role.id, role.title, department.dept_name AS department, role.salary FROM role LEFT JOIN department on role.dept_id = department.id;');
   }
-  
-  searchAllDepartments() {
-    return this.query('SELECT * FROM department;');
-  }
 
+// function to create a new department
   createDept(department) {
     return this.query('INSERT INTO department (dept_name) VALUES ($1)', [department.dept_name]);
   }
 
+//function to create a new role
   createRole(role) {
     const { title, salary, dept_id } = role;
     console.log(role);
@@ -44,7 +45,7 @@ class DB {
     );
   }
 
-  // Create a new employee
+//function to create a new employee
   createEmployee(employee) {
     const { first_name, last_name, role_id, manager_id } = employee;
     return this.query(
@@ -53,6 +54,7 @@ class DB {
     );
   }
 
+// function to update an employee role
   updateEmployeeRole(employeeId, roleId) {
     return this.query('UPDATE employee SET role_id = $1 WHERE id = $2', [
       roleId,
@@ -60,42 +62,14 @@ class DB {
     ]);
   }
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+// function to search employess by deptartment 
   searchEmployeesByDepartment(dept_Id) {
-    console.log("Searching by department for employees: ",dept_Id)
     return this.query(
         "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department ON role.dept_id = department.id WHERE department.id = $1;", 
         [dept_Id]   
          
     ); 
-  }   
-
- 
-}
- 
-   
+  }  
+}   
 
   module.exports = new DB();
-
-
-
-
-//   "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
-
-
-// 'SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department department ON role.dept_id = dept.id WHERE dept.id = $1;',
-//         [dept_id]
